fcd5d15bebb4bf35b22c8928c21b1f3f782bb2c0
AlexisRavel <alexis.ravel@outlook.fr>
2023-02-22 16:52:23 +0100

    Ajout fnct search et fill

--- src/script/autoFillCible.js
+++ src/script/autoFillCible.js
@@ -1,3 +1,5 @@
+const puppeteer = require('puppeteer');
+
 function init() {
     let nom = document.getElementById("formNom").value;
     let numb1 = document.getElementById("formNumb1").value;
@@ -14,6 +16,7 @@ function init() {
 
 class AutoFill {
     data
+    page = null;
 
     constructor(data) {
         this.data = data;
@@ -23,8 +26,31 @@ class AutoFill {
     main() {
         console.log(this.data);
         // Aller chercher le formulaire
+        this.searchFormCible();
         // Le remplir et le soumettre
+        this.fillFormCible();
         // Recuperer ce qu'il renvoie
         // Afficher le resultat
+    }
+
+    async searchFormCible() {
+        // Init de puppeteer
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+
+        // Aller sur la page
+        await page.goto('http://localhost/formCible/form.php');
+        this.page = page;
+    }
+
+    async fillFormCible() {
+        // Remplir et spumettre le formulaire
+        await this.page.type('#formNom', 'Kakapo');
+        await this.page.type('#formMail', 'LeBoss');
+        await this.page.evaluate(() => {
+            document.querySelector('input[value=Envoyer]').click();
+        });
     }
 }
